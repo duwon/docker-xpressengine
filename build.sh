2d5e1f75a3169020100893e40b12d249500734f7
Duwon Jung <duwon@duwon.net>
2020-01-14 07:05:24 +0000

    php-ext-configure gd

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t sunggun/xpressengine:1.9.6 -t sunggun/xpressengine:1.9 -t sunggun/xpressengine:1 ./1.9
+docker build -t duwon/xpressengine:1 ./1.9
 
-docker build -t sunggun/xpressengine:3.0.0-php7 -t sunggun/xpressengine:3.0.0 -t sunggun/xpressengine:3 -t sunggun/xpressengine:latest 3.0
+docker build -t duwon/xpressengine:3 ./3.0
